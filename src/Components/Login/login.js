3387b920ea155bbfd977940ac9695515b721af33
Abhishek <chamoli@Inspiron-7572.localdomain>
2020-06-06 19:55:30 +0530

    save

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './login.scss'
+import img from './logo.jpeg'
+
 export default function Login() {
     return (
         <div>
-            <h1>Login</h1>
+            <img className='img' src={img} alt='logo'/>
+            <h1 className='container signup form-group'>MobbyPark</h1>
+            <div className='container signup form-group' >
+                <input className='form-control' type="text" name="username" placeholder="username"/>
+                <br/>
+                <input className='form-control' type="password" name="password" placeholder="password"/>
+                <br/>
+                <button id='button' className ='btn-primary btn-lg btn-block'>Login </button>
+            </div>
         </div>
     )
 }
--- src/Components/Signup/signup.js
+++ src/Components/Signup/signup.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 import './signup.scss'
+import img from './logo.jpeg'
+
+
 export default function Signup() {
     return (
         <div>
-            <h1>Signup</h1>
+            <img className='img' src={img} alt='logo'/>
+            <div className='container signup form-group' >
+                <h1>MobbyPark</h1>
+                <input className='form-control' type="text" name="username" placeholder="username"/>
+                <br/>
+                <input className='form-control' type="password" name="password" placeholder="password"/>
+                <br/>
+                <button id='button' className ='btn-primary btn-lg btn-block'>Sign up</button>
+            </div>
         </div>
+        
     )
 }
